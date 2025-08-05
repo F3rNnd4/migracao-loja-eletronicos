29fdd78f8da143ea921cdc95476b8c410a51c199
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2025-08-05 11:55:46 -0300

    feat: add products

---
+++ src/data/products.js
@@ -0,0 +1,132 @@
+export const productsData = [
+    {
+      id: 1,
+      title: 'iPhone 15 Pro Max 256GB',
+      price: 'R$ 8.999,00',
+      description: 'O mais avançado iPhone já criado, com chip A17 Pro...',
+      image: '/images/iphone15.jpg',
+      rating: {
+        stars: 5,
+        count: 4.8,
+        reviews: 1234
+      },
+      category: 'smartphones'
+    },
+    {
+      id: 2,
+      title: 'Samsung Galaxy S24 Ultra 512GB',
+      price: 'R$ 7.499,00',
+      description: 'Galaxy AI revolucionário, S Pen integrada...',
+      image: '/images/samsung-galaxy.jpg',
+      rating: {
+        stars: 5,
+        count: 4.7,
+        reviews: 987
+      },
+      category: 'smartphones'
+    },
+    {
+        id: 3,
+        title: 'MacBook Pro 14" M3 Pro 18GB',
+        price: 'R$ 15.999,00',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: 4.9,
+          reviews: 2.156
+        },
+        category: 'Laptops'
+    },
+    {
+        id: 4,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 5,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 6,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 7,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 8,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 9,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+    {
+        id: 10,
+        title: '',
+        price: 'R$ ',
+        description: '',
+        image: '',
+        rating: {
+          stars: 5,
+          count: ,
+          reviews: 
+        },
+        category: ''
+    },
+]
